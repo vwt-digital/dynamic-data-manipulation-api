781a46d326b9ad88b3269830e21afc1b29746368
dvanrhenen <d.vanrhenen@vwt.digital>
2020-06-19 11:56:53 +0200

    Made the OpenApi processing more clear. DAT-3416

--- api_server/openapi_server/openapi_spec.py
+++ api_server/openapi_server/openapi_spec.py
@@ -7,32 +7,43 @@ from functools import reduce
 OPENAPI_PATH = "openapi_server/openapi/openapi.yaml"
 
 
+def get_from_dict(data_dict, map_list):
+    """Returns a dictionary based on a mapping"""
+    return reduce(operator.getitem, map_list, data_dict)
+
+
 def get_specification():
+    """Returns the OpenAPI specification as JSON"""
     with open(OPENAPI_PATH, 'r') as openapi:
         spec = yaml.safe_load(openapi.read())
 
     return spec
 
 
-def get_from_dict(data_dict, map_list):
-    return reduce(operator.getitem, map_list, data_dict)
+def transform_url_rule(url_rule):
+    """Returns the path from the current request"""
+    new_url_rule = str(url_rule).replace('int:', '')
+    new_url_rule = str(new_url_rule).translate(str.maketrans('<>', '{}'))
+
+    return new_url_rule
 
 
-def get_route_reference(route_method, request_method):
-    if request_method in ['get'] and 'responses' in route_method:
+def get_path_schema_reference(path_object, request_method):
+    """Returns the schema-reference from the current path item object"""
+    if request_method in ['get'] and 'responses' in path_object:
         for code in ['200', '201', '202', '203', '204']:
-            if code in route_method['responses']:
+            if code in path_object['responses']:
                 try:
                     route_scheme_ref = get_from_dict(
-                        route_method['responses'][code], ['content', 'application/json', 'schema', '$ref'])
+                        path_object['responses'][code], ['content', 'application/json', 'schema', '$ref'])
                 except KeyError:
                     pass
                 else:
                     return route_scheme_ref
-    elif request_method in ['put', 'post', 'patch'] and 'requestBody' in route_method:
+    elif request_method in ['put', 'post', 'patch'] and 'requestBody' in path_object:
         try:
             route_scheme_ref = get_from_dict(
-                route_method['requestBody'], ['content', 'application/json', 'schema', '$ref'])
+                path_object['requestBody'], ['content', 'application/json', 'schema', '$ref'])
         except KeyError:
             pass
         else:
@@ -40,51 +51,54 @@ def get_route_reference(route_method, request_method):
     return None
 
 
-def get_schema_properties(specification, reference):
+def get_schema(spec, reference):
+    """Returns the schema object based on a reference"""
     if reference and re.search(r"(?:#/)(.+)", reference):
         reference_list = reference.replace('#/', '').split('/')
-        reference_list.append('properties')
 
         try:
-            properties = get_from_dict(specification, reference_list)
+            route_schema = get_from_dict(spec, reference_list)
         except KeyError:
             pass
         else:
-            for field in properties:
-                if properties[field]['type'] in ['array', 'dict']:
-                    for key in properties[field]:
-                        if type(properties[field][key]) == dict and '$ref' in properties[field][key]:
-                            properties[field] = get_schema_properties(specification, properties[field][key]['$ref'])
-                            break
-                else:
-                    properties[field] = properties[field]['type']
-
-            return properties
-    return None
-
-
-def get_route_schema(request, spec, path_schema):
-    if path_schema:
-        request_method = str(request.method).lower()
-        route_reference = get_route_reference(path_schema.get(request_method, None), request_method)
-        route_properties = get_schema_properties(spec, route_reference)
-
-        if route_properties and len(route_properties) > 0:
-            return route_properties
+            return route_schema
 
     return None
 
 
-def transform_url_rule(url_rule):
-    new_url_rule = str(url_rule).replace('int:', '')
-    new_url_rule = str(new_url_rule).translate(str.maketrans('<>', '{}'))
+def get_schema_properties(spec, schema):
+    """Returns all properties as {name: type} from a schema"""
+    properties = schema.get('properties', {})
 
-    return new_url_rule
+    for field in properties:
+        if properties[field]['type'] in ['array', 'dict']:
+            for key in properties[field]:
+                if type(properties[field][key]) == dict and '$ref' in properties[field][key]:
+                    nested_schema = get_schema(spec, properties[field][key]['$ref'])
+                    properties[field] = get_schema_properties(spec, nested_schema)
+                    break
+        else:
+            properties[field] = properties[field]['type']
+
+    return properties if len(properties) > 0 else None
 
 
 def get_database_info(request):
-    spec = get_specification()
-    path_schema = spec.get('paths', {}).get(transform_url_rule(request.url_rule), None)
+    """Returns the 'Database table name' and 'Database keys'"""
+    db_table_name = None
+    db_keys = None
 
-    return path_schema.get('x-db-table-name', None) if path_schema else None, get_route_schema(
-        request, spec, path_schema)
+    spec = get_specification()
+    request_method = str(request.method).lower()
+    path_object = spec.get('paths', {}).get(transform_url_rule(request.url_rule), None)
+
+    if path_object:
+        db_table_name = path_object.get('x-db-table-name', None)
+        path_item_object = path_object.get(request_method, None)
+
+        if path_item_object:
+            path_schema_reference = get_path_schema_reference(path_item_object, request_method)
+            path_schema = get_schema(spec, path_schema_reference)
+            db_keys = get_schema_properties(spec, path_schema)
+
+    return db_table_name, db_keys
