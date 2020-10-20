b570d5c45c7c75937f58abac5b133a9771834021
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2020-10-20 13:00:30 +0200

    Added CacheControl and AuditLog to Docker-container init. DAT-4460

--- api_server/openapi_server/__main__.py
+++ api_server/openapi_server/__main__.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
+import logging
+import os
+
 from . import get_app
+from Flask_AuditLog import AuditLog
+from Flask_No_Cache import CacheControl
+from flask_sslify import SSLify
 
 
 def main():
     app = get_app()
     app.run(port=8080)
+
+    logging.basicConfig(level=logging.INFO)
+
+    AuditLog(app)
+    CacheControl(app)
+    if 'GAE_INSTANCE' in os.environ:
+        SSLify(app, permanent=True)
 
 
 if __name__ == '__main__':
