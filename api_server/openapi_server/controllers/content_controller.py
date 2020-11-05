0dd96325451dd14b2d68b312eb6d324954bcbaf8
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2020-11-05 11:20:26 +0100

    Added support for content type PDF. DAT-4628

--- api_server/openapi_server/controllers/content_controller.py
+++ api_server/openapi_server/controllers/content_controller.py
@@ -3,6 +3,7 @@ import io
 import pandas as pd
 
 from datetime import datetime
+from flask_weasyprint import HTML, render_pdf
 from flask import g, make_response
 
 
@@ -19,7 +20,8 @@ def response_csv(response):
         output.write(csv_response)
 
         response = make_response(output.getvalue())
-        response.headers['Content-Disposition'] = f"attachment; filename={timestamp}.csv"
+        response.headers['Content-Type'] = 'text/csv'
+        response.headers['Content-Disposition'] = f"attachment; filename={g.db_table_name}_{timestamp}.csv"
         return response
     except Exception as e:
         logging.info(f"Generating CSV file failed: {str(e)}")
@@ -40,11 +42,29 @@ def response_xlsx(response):
         writer.save()
 
         response = make_response(output.getvalue())
+        response.headers['Content-Type'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         response.headers['Content-Disposition'] = f"attachment; filename={g.db_table_name}_{timestamp}.xlsx"
         return response
     except Exception as e:
         logging.info(f"Generating XLSX file failed: {str(e)}")
         return make_response('Something went wrong during the generation of a XLSX file', 400)
+
+
+def response_pdf(response):
+    """Returns the data as a PDF file"""
+
+    timestamp = datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
+    try:
+        df = create_dataframe(response)
+        html = df.to_html(escape=False, index=False)
+
+        response = make_response(render_pdf(HTML(string=html), stylesheets=['assets/pdf_styling.css']))
+        response.headers['Content-Type'] = "application/pdf"
+        response.headers["Content-Disposition"] = f"inline; filename={g.db_table_name}_{timestamp}.pdf"
+        return response
+    except Exception as e:
+        logging.info(f"Generating PDF file failed: {str(e)}")
+        return make_response('Something went wrong during the generation of a PDF file', 400)
 
 
 def create_dataframe(response):
@@ -62,5 +82,7 @@ def create_content_response(response, content_type):
         return response_csv(response.get('results', response))
     elif content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':  # XLSX
         return response_xlsx(response.get('results', response))
+    elif content_type == 'application/pdf':  # PDF
+        return response_pdf(response.get('results', response))
 
     return response  # JSON
